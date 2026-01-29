b57b37dab3b605d85f42860f44222b761a809070
prasannakumarSDE <kanakalaprasannakumar@gmail.com>
2026-01-29 14:16:33 +0530

    Add global exception handler for Api Json Resp

--- src/main/java/com/example/pastebin/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/pastebin/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.pastebin.Exception;
 
+
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,15 +10,11 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> badRequest(Exception e) {
-        return Map.of("error", e.getMessage());
-    }
-
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> notFound(Exception e) {
-        return Map.of("error", e.getMessage());
+    public Map<String, String> handleRuntime(RuntimeException ex) {
+        return Map.of(
+                "error", ex.getMessage()
+        );
     }
 }
